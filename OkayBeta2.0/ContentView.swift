c1f36790bdc8bde2dfca79fb9be18e1827044798
jPraiser23 <praiserjohn@gmail.com>
2024-07-09 10:25:20 -0700

    first commit

--- OkayBeta2.0/ContentView.swift
+++ OkayBeta2.0/ContentView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+
+            ScrollView{
+                VStack{
+                    Picker(selection: $isLoginMode, label:
+                            Text("Picker here")){
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                        
+                    Button{
+                        
+                    } label:{
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 64))
+                            .padding()
+                    }
+                    
+                    TextField("Email", text : $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                    SecureField("Password", text : $password)
+                    Button {
+                        
+                    }label:{
+                        HStack{
+                            Spacer()
+                            Text("Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                            Spacer()
+                        }.background(Color.blue)
+                    }
+                    
+                }.padding()
+  
+            }
+            .navigationTitle("Create Account")
         }
+        
         .padding()
     }
 }
