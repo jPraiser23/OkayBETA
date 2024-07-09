8a017ca4ccace75cd65b11f898f043dbd8de9a5d
jPraiser23 <praiserjohn@gmail.com>
2024-07-09 07:56:12 -0700

    Initial Commit

---
+++ OkayBeta2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OkayBeta2.0
+//
+//  Created by mac on 7/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OkayBeta2.0/OkayBeta2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  OkayBeta2_0App.swift
+//  OkayBeta2.0
+//
+//  Created by mac on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct OkayBeta2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
